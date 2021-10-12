b281d4d42d97cec4bf8ea2e84883fce1fd3ed2be
isadoratavare <isadoratavare85@gmail.com>
2021-10-12 10:31:02 -0300

    função pausar e zerar finalizadas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,35 @@
-let counterS = 0;
-let counterMin = 0;
-let timer = 0;
 function addZero(counterS){
     return (counterS<10) ? `0${counterS}` : counterS
 }
-function iniciar(counterS, countSeg,counterM,countMin){
-    timer = setInterval(function (){
-        counterS += 1;
-        countSeg.innerHTML = addZero(counterS);
-        if(counterS === 59){
-            counterM  +=1
-            countMin.innerHTML = addZero(counterM);
-            counterS -= 60
+function toMinutes(elementMinutes){
+    counter -= 60;
+    counterMinutes ++;
+    elementMinutes.innerHTML = addZero(counterMinutes)
+}
+function play(elementSeconds,elementMinutes){
+    clearInterval(timer)
+    timer = setInterval(function(){
+        counter++;
+        elementSeconds.innerHTML = addZero(counter);
+        if (counter === 60){
+            toMinutes(elementMinutes)
         }
-    }, 1000
-    );
-    return timer
+    },1000)
 }
-function zerar(timer, counterS, countSeg,counterM,countMin){
-    setTimeout(function() {
-        clearInterval(timer)
-    })
-    counterS = 0; 
-    counterM = 0;
-    countSeg.innerHTML = '00'
-    countMin.innerHTML = "00"
+function pausar() {
+    clearInterval(timer)
 }
+function zerar(elementSeconds, elementMinutes){
+    clearInterval(timer)
+    counter = 0; 
+    counterMinutes = 0;
+    elementMinutes.innerHTML = '00'
+    elementSeconds.innerHTML = "00"
+}
+let counter = 0;
+let counterMinutes = 0;
+let elementSeconds = document.querySelector('#seg');
+let elementMinutes = document.querySelector('#min');
+let timer;
+
+
