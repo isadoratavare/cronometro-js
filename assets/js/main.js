a13daa697191c511494a20bc1f4b873e936448ff
isadoratavare <isadoratavare85@gmail.com>
2021-10-13 11:45:41 -0300

    melhorando css e js

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,25 +11,24 @@ function play(elementSeconds,elementMinutes){
     timer = setInterval(function(){
         counter++;
         elementSeconds.innerHTML = addZero(counter);
-        if (counter === 60){
+        if (counter === 59){
             toMinutes(elementMinutes)
         }
     },1000)
 }
-function pausar() {
+function pause() {
     clearInterval(timer)
 }
-function zerar(elementSeconds, elementMinutes){
+function reset(elementSeconds, elementMinutes){
     clearInterval(timer)
     counter = 0; 
     counterMinutes = 0;
     elementMinutes.innerHTML = '00'
     elementSeconds.innerHTML = "00"
 }
+
 let counter = 0;
 let counterMinutes = 0;
 let elementSeconds = document.querySelector('#seg');
 let elementMinutes = document.querySelector('#min');
 let timer;
-
-
